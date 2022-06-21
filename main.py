52eda1a63210b9f5f04d9f402bf4324c4b3e7df3
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2022-06-21 17:30:05 +0200

    Primera Solución (Falta testear)

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def numberOfAvailableDifferentPaths(board, snake, depth):
+    # Crear y rellenar la matriz que representa el tablero
+    n, m = board
+    board_matrix = np.zeros([n, m], dtype=int)  # boardMatrix = [[0]*m for i in range(n)]
+    for body_part in snake:
+        board_matrix[body_part[0], body_part[1]] = 1
+
+    return numberOfAvailableDifferentPaths_aux(board, snake, depth, board_matrix, 0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def numberOfAvailableDifferentPaths_aux(board, snake, depth, board_matrix, current_depth):
+    n, m = board
+    cells = n * m
+    head = snake[0]
+    tail = snake[len(snake)-1]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Si se ha alcanzado un camino de longitud = depth, se suma un camino encontrado
+    if current_depth == depth:
+        return 1
+
+    else:
+        different_paths = 0
+        movements = [[head[0]-1, head[1]], [head[0]+1, head[1]], [head[0], head[1]+1], [head[0], head[1]-1]]
+
+        # Se mueve la serpiente en las direcciones posibles
+        for new_head in movements:
+            if n > new_head[0] and new_head[0] >= 0 and m > new_head[1] and new_head[1] >= 0 and board_matrix[new_head[0], new_head[1]] == 0:
+                # Efectuar movimiento
+                board_matrix[tail[0], tail[1]] = 0
+                board_matrix[new_head[0], new_head[1]] = 1
+                old_tail = snake[-1]
+                snake = [new_head] + snake[:-1]
+
+                # Calcular el resto del camino
+                different_paths += numberOfAvailableDifferentPaths_aux(board, snake, depth, board_matrix, current_depth+1)
+
+                # Deshacer este movimiento para poder hacer otras llamadas recursivas
+                board_matrix[tail[0], tail[1]] = 1
+                board_matrix[new_head[0], new_head[1]] = 0
+                snake = snake[1:] + [old_tail]
+
+        return different_paths
+
+
+board = [4, 3]
+snake = [[2, 2], [3, 2], [3, 1], [3, 0], [2, 0], [1, 0], [0, 0]]
+depth = 3
+
+print(numberOfAvailableDifferentPaths(board, snake, depth))
